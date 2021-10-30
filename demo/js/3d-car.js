9d0c13d4837ef8370a209cf30deb059e42ce35d6
Chirag Mistry <chiragmistry@goldenmace.com>
2021-10-30 19:28:08 +0530

    -update enviroment light

--- demo/js/3d-car.js
+++ demo/js/3d-car.js
@@ -4,8 +4,10 @@ let render
 let car_model
 let container
 let grid;
+const wheels = [];
 function init() {
     container = document.querySelector('.scene')
+    console.log(container.offsetWidth)
 
     scene = new THREE.Scene()
     const fov = 40
@@ -24,24 +26,75 @@ function init() {
     grid.material.transparent = true;
     scene.add( grid );
 
-    const light = new THREE.DirectionalLight(0xFFFFFF, 2.1)
-    light.position.set(40, 8, 50)
-    scene.add(light)
+    const bodyMaterial = new THREE.MeshPhysicalMaterial( {
+        color: 0xff0000, metalness: 0.6, roughness: 0.4, clearcoat: 0.05, clearcoatRoughness: 0.05
+    } );
+
+    const detailsMaterial = new THREE.MeshStandardMaterial( {
+        color: 0xffffff, metalness: 1.0, roughness: 0.5
+    } );
+
+    const glassMaterial = new THREE.MeshPhysicalMaterial( {
+        color: 0xffffff, metalness: 0, roughness: 0.1, transmission: 0.5, transparent: true
+    } );
+
+    const bodyColorInput = document.getElementById( 'body-color' );
+    bodyColorInput.addEventListener( 'input', function () {
+
+        bodyMaterial.color.set( this.value );
+
+    } );
+
+    const detailsColorInput = document.getElementById( 'details-color' );
+    detailsColorInput.addEventListener( 'input', function () {
+
+        detailsMaterial.color.set( this.value );
+
+    } );
+
+    const glassColorInput = document.getElementById( 'glass-color' );
+    glassColorInput.addEventListener( 'input', function () {
+
+        glassMaterial.color.set( this.value );
+
+    } );
+
 
     render = new THREE.WebGLRenderer({antialias: true, alpha: true})
     render.setSize(container.clientWidth, container.clientHeight)
     render.setPixelRatio(window.devicePixelRatio)
     render.setAnimationLoop( render );
     container.appendChild(render.domElement)
+
+    const pmremGenerator = new THREE.PMREMGenerator( render );
+    scene.environment = pmremGenerator.fromScene( new THREE.RoomEnvironment() ).texture;
+
+
     const dracoLoader = new THREE.DRACOLoader();
     dracoLoader.setDecoderPath( 'js/draco/gltf/' );
     let loader = new THREE.GLTFLoader()
     loader.setDRACOLoader( dracoLoader );
     const shadow = new THREE.TextureLoader().load( '3d/car/ferrari_ao.png' );
-    loader.load('./3d/ferrari0.glb', function (gltf) {
+    loader.load('./3d/ferrari.glb', function (gltf) {
         car_model = gltf.scene.children[0]
         console.log(car_model.getObjectByName( 'body' ))
-        car_model.scale.set(.1, .1, .1)
+        // car_model.scale.set(.1, .1, .1)
+        car_model.getObjectByName( 'body' ).material = bodyMaterial;
+
+        car_model.getObjectByName( 'rim_fl' ).material = detailsMaterial;
+        car_model.getObjectByName( 'rim_fr' ).material = detailsMaterial;
+        car_model.getObjectByName( 'rim_rr' ).material = detailsMaterial;
+        car_model.getObjectByName( 'rim_rl' ).material = detailsMaterial;
+        car_model.getObjectByName( 'trim' ).material = detailsMaterial;
+
+        car_model.getObjectByName( 'glass' ).material = glassMaterial;
+
+        wheels.push(
+            car_model.getObjectByName( 'wheel_fl' ),
+            car_model.getObjectByName( 'wheel_fr' ),
+            car_model.getObjectByName( 'wheel_rl' ),
+            car_model.getObjectByName( 'wheel_rr' )
+        );
         const mesh = new THREE.Mesh(
             new THREE.PlaneGeometry( 0.655 * 4, 1.3 * 4 ),
             new THREE.MeshBasicMaterial( {
@@ -65,16 +118,7 @@ function init() {
 }
 function animate() {
     window.requestAnimationFrame(animate)
-    car_model.rotation.y += .020
+    car_model.rotation.y += .010
     render.render(scene, camera)
 }
-
-document.addEventListener('mousemove', (e) => {
-    // house.rotation.y = .8 * (e.clientX / window.innerWidth)
-    // house.rotation.x = .5 * (e.clientX / window.innerHeight)
-    
-    // house.position.y = (e.clientX / window.innerWidth) * 1.5
-
-    // house.position.x = (e.clientX / window.innerHeight) * -.5
-})
 init()
